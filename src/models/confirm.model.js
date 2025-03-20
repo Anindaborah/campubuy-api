917ade22c29a6d9f180ef2a4f1d26fab107b65c0
Anindaborah <anindaborah123@gmail.com>
2025-03-20 23:12:33 +0530

    initial commit

---
+++ src/models/confirm.model.js
@@ -0,0 +1,45 @@
+import mongoose , {Schema} from "mongoose";
+const User=require("./User");
+const Product=require("./Product");
+
+const confirmSchema=new Schema({
+
+      user:{
+         type:Schema.Types.ObjectId,
+         ref:"User"
+         },
+      confirmItems:[{
+         product:{type:Schema.Types.ObjectId,
+         ref:"Product"
+         },
+         name:{
+            type: String,
+            required: true
+          },
+          description: {
+            type: String,
+            required: true
+          },
+          photo: {
+            type: String,
+            required: true
+          },
+          quantity: {
+            type: Number,
+            required: true
+          },
+          price: {
+            type: Number,
+            required: true
+          },
+          totalPrice: {
+            type: Number,
+            required: true
+          }
+      }],
+      },
+    {
+        timestamps:true,
+    });
+
+    module.exports=mongoose.model("Confirm",confirmSchema);
